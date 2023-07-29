0c0ed44e1f3d57ff2d0ac33ba19b9576e1a77e7c
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2023-07-28 22:49:15 -0400

    file upload button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [file, setFile] = useState();
+  const [uploadedFile, setUploadedFile] = useState();
+  const [error, setError] = useState();
+
+  function handleChange(event) {
+    setFile(event.target.files[0]);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    const url = 'http://localhost:3000/upload';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', file.name);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
+    };
+    axios.post(url, formData, config)
+      .then((response) => {
+        console.log(response.data);
+        setUploadedFile(response.data.file);
+      })
+      .catch((error) => {
+        console.error("Error uploading file: ", error);
+        setError(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleSubmit}>
+          <h1>React File Upload</h1>
+          <input type="file" onChange={handleChange}/>
+          <button type="submit">Upload</button>
+        </form>
+        {uploadedFile && <img src={uploadedFile} alt="Uploaded content"/>}
+        {error && <p>Error uploading file: {error.message}</p>}
     </div>
   );
 }
